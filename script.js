09115df90ddb1288f774c8618389d7802bdac075
Danila <danchikplay@bk.ru>
2024-12-26 15:46:40 +0300

    рабочий список

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const buttonElement = document.querySelector(".button");
 const textElement = document.querySelector(".text");
 const boxColorElement = document.querySelector(".box-color");
 const hotbarElement = document.querySelector(".hotbar");
-const colorDivElement = document.querySelectorAll(".coloDiv");
+const colorDivElements = document.querySelectorAll(".colorDiv");
 
 let color = "#000000";
 
 class randomColor {
-  letters = "1234567890ABCDEF";
+  letters = "1234567890abcdef";
   color = "#";
   constructor() {
     for (let i = 0; i < 6; i++) {
@@ -24,6 +24,15 @@ buttonElement.addEventListener("click", () => {
   colorDiv.style.background = color;
   colorDiv.classList.add("colorDiv");
   hotbarElement.append(colorDiv);
+  colorDiv.addEventListener("click", (event) => {
+    color = event.target.style.background
+      .replace("rgb(", "")
+      .replace(")", "")
+      .split(", ");
+    color = rgbToHex(color[0], color[1], color[2]);
+    textElement.textContent = color;
+    boxColorElement.style.background = color;
+  });
 });
 
 textElement.addEventListener("click", () => {
@@ -33,7 +42,3 @@ textElement.addEventListener("click", () => {
 function rgbToHex(r, g, b) {
   return "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
 }
-
-colorDivElement.forEach((element) => {
-  element.addEventListener("click", () => {});
-});
